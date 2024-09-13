1dfa08abf00540a217ead50cbd3235887f4546b2
Michael Akoko <akokomichael37@gmail.com>
2024-09-13 17:02:24 +0300

    include script files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { Outlet } from "react-router-dom";
+// App.js
+import { useState, useEffect } from "react";
+import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  // Add 'navigate' to the dependency array to fix the warning
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      {/* Add conditional rendering so users have to be logged in to see pages on the site */}
+      {isLoggedIn ? <NavBar logout={logout} /> : <Navigate to="/login" />}
+      <Outlet context={login} />
     </div>
   );
 }
